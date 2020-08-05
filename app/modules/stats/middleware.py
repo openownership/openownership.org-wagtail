50ade4ec521da2183f3bdf4e4b5ae2a9ff4f7e66
Andy Beaumont <andy@andybeaumont.com>
2020-08-05 18:31:21 +0100

    ViewCountMiddleware no longer breaks Wagtail Admin.

--- app/modules/stats/middleware.py
+++ app/modules/stats/middleware.py
@@ -1,22 +1,26 @@
-from django.http import Http404
-from django.utils.deprecation import MiddlewareMixin
-from consoler import console
 from wagtail.core.models import Site
 from modules.stats.models import ViewCount
 
 
-class ViewCountMiddleware(MiddlewareMixin):
+class ViewCountMiddleware:
+
+    def __init__(self, get_response):
+        self.get_response = get_response
+        self.skip = ['admin', 'static', 'media']
+        # TODO: Make the skip list into an overridable setting
+
+    def __call__(self, request):
+        page = self._find_page_from_path(request, request.path)
+        if page and hasattr(page, 'is_countable') and page.is_countable is True:
+            ViewCount.objects.hit(page.id)
+
+        response = self.get_response(request)
+        return response
 
     def _find_page_from_path(self, request, path):
         site = Site.find_for_request(request)
-        if not site:
-            raise Http404
-
         path_components = [component for component in path.split('/') if component]
+        if path_components[0] in self.skip:
+            return
         page, args, kwargs = site.root_page.specific.route(request, path_components)
         return page
-
-    def process_request(self, request):
-        page = self._find_page_from_path(request, request.path)
-        if hasattr(page, 'is_countable') and page.is_countable is True:
-            ViewCount.objects.hit(page.id)
