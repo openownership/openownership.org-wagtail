de193c1789a5efcc0853083b6e1150e6392ec4d3
Phil Gyford <phil@gyford.com>
2022-02-08 12:21:39 +0000

    Remove duplicate TaxonomyObjectMixin

--- app/modules/content/models/taxonomy.py
+++ app/modules/content/models/taxonomy.py
@@ -4,26 +4,7 @@ from django_extensions.db.fields import AutoSlugField
 from modelcluster.models import ClusterableModel
 from wagtail.admin.edit_handlers import FieldPanel
 
-
-class TaxonomyObjectMixin(ClusterableModel):
-
-    class Meta():
-        ordering = ['name', ]
-        abstract = True
-
-    name = models.CharField('Name', max_length=255)
-    slug = AutoSlugField('Slug', populate_from='name')
-    autocomplete_search_field = 'name'
-
-    panels = [
-        FieldPanel('name'),
-    ]
-
-    def __str__(self):
-        return self.name
-
-    def autocomplete_label(self):
-        return self.name
+from modules.content.models.mixins import TaxonomyObjectMixin
 
 
 class NewsCategory(TaxonomyObjectMixin):
