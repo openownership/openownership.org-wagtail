b3c5b6a6c8439e057862acc300b3f8a03408ef9a
Ana Rodrigues <ana.rdrg@gmail.com>
2022-02-28 15:47:12 +0000

    #25 ~ tidy up page layout for the blog and news index page;

--- app/assets/_dev/js/components/share-page.js
+++ app/assets/_dev/js/components/share-page.js
@@ -1,19 +1,21 @@
 var sharePageButton = document.getElementById('share-page');
 var sharePageLinks = document.getElementById('share-page-links');
 
-sharePageButton.addEventListener('click', (e) => {
-    e.preventDefault();
-    
-    sharePageButton.classList.toggle('expanded');
-
-    sharePageLinks.classList.toggle('collapsed');
-
-    if (sharePageButton.classList.contains('expanded')) {
-        sharePageButton.setAttribute('aria-expanded', true);
+if (sharePageButton) {
+    sharePageButton.addEventListener('click', (e) => {
+        e.preventDefault();
         
-    } else {
-        sharePageButton.setAttribute('aria-expanded', false);
-     
-    }
-  })
+        sharePageButton.classList.toggle('expanded');
+    
+        sharePageLinks.classList.toggle('collapsed');
+    
+        if (sharePageButton.classList.contains('expanded')) {
+            sharePageButton.setAttribute('aria-expanded', true);
+            
+        } else {
+            sharePageButton.setAttribute('aria-expanded', false);
+         
+        }
+    }); 
+}
 
