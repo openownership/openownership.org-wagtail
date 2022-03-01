341addcaebbc43a50492425fe3ead2b9427e3de0
Andy Beaumont <andy@andybeaumont.com>
2022-03-01 18:56:05 +0000

    Configure the AWS bucket and CDN in staging settings.

--- app/config/settings/staging.py
+++ app/config/settings/staging.py
@@ -1,11 +1,16 @@
+import os
+import envkey  # NOQA
 from .base import *  # NOQA
 from .remote import *  # NOQA
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-BASE_URL = 'https://staging.openownership.org'
+BASE_URL = 'https://openownership.hactar.work'
 DOMAIN_NAME = f'staging.{BASE_URL}'
+
+AWS_S3_CUSTOM_DOMAIN = 'oownershipstage-1b54.kxcdn.com'
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_BUCKET')
 
 MEDIA_ROOT = 'staging/media/'
 
