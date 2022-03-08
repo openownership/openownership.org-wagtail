7daedb04452662c363e3a602a72b142a32cce153
Ana Rodrigues <ana.rdrg@gmail.com>
2022-03-08 17:44:30 +0000

    #13 ~ hide dropdown menu from desktop view; fix styles in mobile view

--- app/assets/_dev/js/components/navigation.js
+++ app/assets/_dev/js/components/navigation.js
@@ -21,12 +21,16 @@ var firstLevelButtons = document.querySelectorAll('.--level-one');
 firstLevelButtons.forEach(function(button) {
     button.addEventListener('click', function(){
         let submenu = button.nextElementSibling;
+        var parentLi = button.parentElement;
+  
         if (submenu.classList.contains('subnav--hidden')) {
           this.setAttribute('aria-expanded', 'true');
           this.classList.add('expanded');
           submenu.classList.remove('subnav--hidden');
+          parentLi.classList.add('expanded');
         } else {
           submenu.classList.add('subnav--hidden'); 
+          parentLi.classList.remove('expanded');
           this.classList.remove('expanded');
           this.setAttribute('aria-expanded', 'false');
         }
