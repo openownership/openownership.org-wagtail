707bfecb9e9bfeadc55eb2e075817dc72faaa0e2
Ana Rodrigues <ana.rdrg@gmail.com>
2022-03-09 16:08:42 +0000

    #13 ~ add expand icons;

--- app/assets/_dev/js/components/navigation.js
+++ app/assets/_dev/js/components/navigation.js
@@ -3,12 +3,16 @@ var secondaryLevelButtons = document.querySelectorAll('.--level-two');
 secondaryLevelButtons.forEach(function(button) {
     button.addEventListener('click', function(){
         let submenu = button.nextElementSibling;
+        var parentLi = button.parentElement;
+
         if (submenu.classList.contains('subnav--hidden')) {
           this.setAttribute('aria-expanded', 'true');
           this.classList.add('expanded');
           submenu.classList.remove('subnav--hidden');
+          parentLi.classList.add('expanded');
         } else {
           submenu.classList.add('subnav--hidden'); 
+          parentLi.classList.remove('expanded');
           this.classList.remove('expanded');
           this.setAttribute('aria-expanded', 'false');
         }
