d3d7e1619b203f793d93bbd18eee14d6bc05ea76
helau64 <heather@hactar.is>
2022-10-28 12:08:23 +0100

    Replace precss with seperate modules to solve security vulnerability

--- app/assets/postcss.config.js
+++ app/assets/postcss.config.js
@@ -3,11 +3,16 @@ module.exports = {
     syntax: 'postcss-scss',
     plugins: [
         require('postcss-easy-import'),
-        require('precss'),
+        require('postcss-advanced-variables'),
+        require('postcss-atroot'),
+        require('postcss-extend-rule'),
+        require('postcss-preset-env'),
+        require('postcss-property-lookup'),
         require('postcss-rem')({baseline: 18, fallback: true}),
         require('postcss-focus-visible'),
         require('postcss-map-get'),
         require('postcss-css-variables'),
+        require('tailwindcss/nesting'),
         require('tailwindcss'),
         require('autoprefixer')({
             grid: 'autoplace'
