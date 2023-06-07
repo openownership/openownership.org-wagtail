db8525860d54dfacfad579f4d5d29d88aff19a1b
Ana Rodrigues <ana.rdrg@gmail.com>
2023-06-07 13:57:04 +0100

    #271 ~ show/hide functionality when "other" options are selected;

--- app/assets/_dev/js/components/feedback-form.js
+++ app/assets/_dev/js/components/feedback-form.js
@@ -1,21 +1,84 @@
 var giveFeedbackButton = document.getElementById('give-feedback');
 var feedbackForm = document.getElementById('feedback-form');
 
+var textAreaWhyOther = document.getElementById('text-area-why-other');
+var textAreaWhereOther = document.getElementById('text-area-where-other');
+
+var whyOtherRadioButton = document.getElementById('id_why_downloading_4');
+var whereOtherRadioButton = document.getElementById('id_where_downloading_4');
+
 if (giveFeedbackButton) {
     giveFeedbackButton.addEventListener('click', (e) => {
         e.preventDefault();
-        
         giveFeedbackButton.classList.toggle('expanded');
-    
-        feedbackForm.classList.toggle('collapsed');
-    
+
+        // check if "other" is selected in the "why" feedback and show/hide the text area field
+        var whyDownloadingHandler = function expandCollapseOtherField(event) {
+            if (document.querySelector('input[name="why_downloading"]:checked').value === 'other') {
+                if (!textAreaWhyOther.classList.contains('expanded')) {
+                    textAreaWhyOther.classList.add('expanded');
+                }
+            } else {
+                if (textAreaWhyOther.classList.contains('expanded')) {
+                    textAreaWhyOther.classList.remove('expanded');
+                }
+            }
+        }
+
+        // check if "other" is selected in the "where" feedback and show/hide the text area field
+        var whereWorkerHandler = function expandCollapseOtherField(event) {
+            if (document.querySelector('input[name="where_work"]:checked').value === 'other') {
+                if (!textAreaWhereOther.classList.contains('expanded')) {
+                    textAreaWhereOther.classList.add('expanded');
+                }
+            } else {
+                if (textAreaWhereOther.classList.contains('expanded')) {
+                    textAreaWhereOther.classList.remove('expanded');
+                }
+            }
+        }
+        
+        //show/hide form
         if (giveFeedbackButton.classList.contains('expanded')) {
-            giveFeedbackButton.setAttribute('aria-expanded', true);
             
+            giveFeedbackButton.setAttribute('aria-expanded', true);
+            feedbackForm.classList.remove('collapsed');
+
+            //attach event listener to the radio buttons and check if "other" was chosen
+            document.querySelectorAll("input[name='why_downloading']").forEach((input) => {
+                if (!input.getAttribute('data-event-listener')) {
+                    input.setAttribute('data-event-listener', 'true');
+                    input.addEventListener('change', whyDownloadingHandler, true);
+                }
+            });
+
+            document.querySelectorAll("input[name='where_work']").forEach((input) => {
+                if (!input.getAttribute('data-event-listener')) {
+                    input.setAttribute('data-event-listener', 'true');
+                    input.addEventListener('change', whereWorkerHandler, true);
+                }
+            });
+
         } else {
             giveFeedbackButton.setAttribute('aria-expanded', false);
-         
+
+            //remove event listeners
+            document.querySelectorAll("input[name='why_downloading']").forEach((input) => {
+                input.setAttribute('data-event-listener', 'false');
+                input.removeEventListener('change', whyDownloadingHandler, true);
+            });
+
+            document.querySelectorAll("input[name='where_work']").forEach((input) => {
+                input.setAttribute('data-event-listener', 'false');
+                input.removeEventListener('change', whereWorkerHandler, true);
+            });
+
+            feedbackForm.classList.add('collapsed');
         }
     }); 
 }
+
+
+
+
 
