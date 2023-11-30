26f1e16a4fbe885327daec6b4f7c43c011b3a5fb
Andy Beaumont <andy@andybeaumont.com>
2023-11-30 11:57:19 +0000

    Reorganise the cron tasks, fix check notion access management command. #432

---
+++ app/modules/notion/cron/regimes.py
@@ -0,0 +1,209 @@
+# 3rd party
+from consoler import console
+from django.conf import settings
+from django_cron import Schedule
+
+# Module
+from .helpers import check_headers
+from modules.notion.data import DISCLOSURE_REGIMES
+from modules.notion.models import CoverageScope, DisclosureRegime
+from modules.notion.cron.base import NotionCronBase, NotionError
+
+
+class SyncRegimes(NotionCronBase):
+    RUN_EVERY_MINS = 120  # every 2 hours
+
+    schedule = Schedule(run_every_mins=RUN_EVERY_MINS)
+    code = 'notion.sync_regimes'
+
+    def __init__(self, *args, **kwargs):
+        self._model = DisclosureRegime
+        super().__init__(*args, **kwargs)
+
+    def do(self, data: dict = None, force: bool = False):
+        """Sync regimes from Notion
+
+        Args:
+            data (dict, optional): We're only going to pass the data in here in tests
+        """
+
+        # The ID we have for DISCLOSURE_REGIMES is already the DB id
+        if not data:
+            data = self.fetch_all_data(DISCLOSURE_REGIMES)
+
+        if check_headers("Disclosure Regime", data):
+            results = data.get('results', [])
+            if len(results):
+                # Do stuff here to save the data from Notion
+                for item in results:
+                    self._handle_regime(item, force)
+            else:
+                # Notify of failure, probably Slack and logging
+                console.warn("Regimes - Results was zero len")
+
+            self._clean_up(data)
+
+    def _handle_regime(self, regime: dict, force: bool = False) -> bool:
+        """Gets data from notion (`regime`) and saves it as a DisclosureRegime.
+
+        Args:
+            regime (dict): The OG Notion data, as a dict for a single regime row
+
+        Returns:
+            bool: Success / Failure
+        """
+        # import ipdb; ipdb.set_trace()
+        try:
+            notion_id = regime['id']
+        except KeyError:
+            console.warn(regime)
+            console.error("No notion ID found")
+
+        country_id = self._get_rel_id(regime, 'Country')
+        if country_id is None:
+            return False
+
+        country = None
+        if country_id:
+            country = self._get_country(country_id)
+
+        if country:
+            obj, created = DisclosureRegime.objects.get_or_create(
+                notion_id=notion_id,
+                country=country,
+            )
+        else:
+            console.warn("No related country found, skipping")
+            return False
+
+        if not self._is_updated(obj, regime) and not force:
+            return False
+
+        # This is either a new row, or it has been updated, so save stuff
+        self._set_universals(obj, regime)
+        obj.title = self._get_title(regime)
+        obj.definition_legislation_url = self._get_value(
+            regime, '1.1 Definition: Legislation URL',
+        )
+        obj.coverage_legislation_url = self._get_value(
+            regime, '2.3 Coverage: Legislation URL',
+        )
+        scope_tags = self._get_scope_tags(regime)
+        if scope_tags:
+            for item in scope_tags:
+                obj.coverage_scope.add(item)
+                obj.coverage_scope.commit()
+
+        obj.stage = self._get_stages(regime)  # 0 Stage - multi_select
+        obj.central_register = self._get_value(regime, '4.1 Central register')
+        obj.public_access = self._get_value(regime, '5.1 Public access')
+        obj.public_access_register_url = self._get_value(
+            regime, '5.1.1 Public access: Register URL')
+        obj.year_launched = self._get_value(regime, '4.2 Year launched')
+        obj.structured_data = self._get_value(regime, '6.1 Structured data')
+        obj.api_available = self._get_value(regime, '6.3 API available')
+        obj.data_in_bods = self._get_value(regime, '6.4 Data published in BODS')
+        obj.on_oo_register = self._get_value(regime, '6.5 Data on OO Register')
+        obj.legislation_url = self._get_value(regime, '8.4 Legislation URL')
+        obj.threshold = str(self._get_value(regime, '1.2 Threshold'))
+        # New legislation fields
+        obj.sufficient_detail_legislation_url = self._get_value(
+            regime, "3.1 Sufficient detail: Legislation URL",
+        )
+        obj.public_access_protection_regime_url = self._get_value(
+            regime, "5.4.1 Protection regime URL",
+        )
+        obj.public_access_legal_basis_url = self._get_value(
+            regime, "5.5 Legal basis for publication URL",
+        )
+        obj.sanctions_enforcement_legislation_url = self._get_value(
+            regime, "9 Sanctions and enforcement: Legislation URL",
+        )
+
+        try:
+            obj.save()
+        except Exception as e:
+            console.error("Failed to save")
+            console.error(e)
+            if settings.DEBUG:
+                import ipdb
+                ipdb.set_trace()
+
+    def _get_scope_tags(self, data: dict) -> list:
+        """Takes this dict, creates a CoverageScope tag for each `name` and returns a list of them
+
+        '2.1 Coverage: Scope': {
+            'id': 'Z%3Dqf',
+            'type': 'multi_select',
+            'multi_select': [
+                {
+                    'id': 'd084a8fc-fbbc-4d63-b184-b6ab5717e644',
+                    'name': 'Full-economy',
+                    'color': 'green'
+                },
+                {
+                    'id': '859da0c6-cb5b-40f8-b238-8c7e913fb3e5',
+                    'name': 'Sectoral: Extractives',
+                    'color': 'purple'
+                }
+            ]
+        }
+
+        Args:
+            data (dict): The disclosure regime row
+
+        Returns:
+            list: List of tags
+        """
+        try:
+            tags = []
+            scopes = data['properties']['2.1 Coverage: Scope']['multi_select']
+            if not len(scopes):
+                return None
+
+            for item in scopes:
+                tag, created = CoverageScope.objects.get_or_create(name=item['name'])
+                tags.append(tag)
+
+        except Exception as err:
+            console.warn("Failed to add scope tags")
+            console.warn(err)
+            f"Failed to add scope tags - {err}"
+            raise NotionError() from err
+        else:
+            # console.info(f"Scope tags: {tags}")
+            return tags
+
+    def _get_stages(self, data: dict) -> list:
+        """Takes this dict, returns a comma separated list of values
+
+        '0 Stage': {
+            'id': '86%605',
+            'type': 'multi_select',
+            'multi_select': [
+                {
+                    'id': '486a37df-ec33-4f48-a513-c734ec2d7d8c',
+                    'name': 'Systems',
+                    'color': 'yellow'
+                }
+            ]
+        },
+
+        Args:
+            data (dict): The disclosure regime row
+
+        Returns:
+            list: List of tags
+        """
+        try:
+            stages = data['properties']['0 Stage']['multi_select']
+            if not len(stages):
+                return None
+
+            return ', '.join([item['name'] for item in stages])
+
+        except Exception as err:
+            console.warn("Failed to get Stages")
+            console.warn(err)
+            f"Failed to get Stages - {err}"
+            raise NotionError() from err
--- app/modules/notion/helpers.py
+++ app/modules/notion/helpers.py
@@ -1,5 +1,4 @@
 # stdlib
-import json
 
 # 3rd party
 from cacheops import cached
@@ -10,47 +9,52 @@ from modules.bots.notionbot import notionbot
 from modules.notion.models import CountryTag
 
 
-HEADERS_COUNTRY_TRACKER = [
-    'OO Support', 'ISO2'
-]
+HEADERS_COUNTRY_TRACKER = ["OO Support", "ISO2"]
 
 
 HEADERS_COMMITMENT_TRACKER = [
-    'Country', 'Date', 'Link', 'Commitment type',
-    'Central register', 'Public register', 'Summary Text', 'All sectors'
+    "Country",
+    "Date",
+    "Link",
+    "Commitment type",
+    "Central register",
+    "Public register",
+    "Summary Text",
+    "All sectors",
 ]
 
 
 HEADERS_DISCLOSURE_REGIMES = [
-    'Country', '1.1 Definition: Legislation URL', '2.3 Coverage: Legislation URL',
-    '4.1 Central register', '5.1 Public access', '5.1.1 Public access: Register URL',
-    '4.2 Year launched', '6.1 Structured data', '6.3 API available', '6.4 Data published in BODS',
-    '6.5 Data on OO Register', '8.4 Legislation URL', '1.2 Threshold',
-    "3.1 Sufficient detail: Legislation URL", "5.4.1 Protection regime URL",
-    "5.5 Legal basis for publication URL", "9 Sanctions and enforcement: Legislation URL",
-    '2.1 Coverage: Scope', '0 Stage'
+    "Country",
+    "1.1 Definition: Legislation URL",
+    "2.3 Coverage: Legislation URL",
+    "4.1 Central register",
+    "5.1 Public access",
+    "5.1.1 Public access: Register URL",
+    "4.2 Year launched",
+    "6.1 Structured data",
+    "6.3 API available",
+    "6.4 Data published in BODS",
+    "6.5 Data on OO Register",
+    "8.4 Legislation URL",
+    "1.2 Threshold",
+    "3.1 Sufficient detail: Legislation URL",
+    "5.4.1 Protection regime URL",
+    "5.5 Legal basis for publication URL",
+    "9 Sanctions and enforcement: Legislation URL",
+    "2.1 Coverage: Scope",
+    "0 Stage",
 ]
 
 
 def check_headers(tracker: str, data: dict) -> bool:
     if tracker == "Country":
-        return _check_headers(
-            HEADERS_COUNTRY_TRACKER,
-            data,
-            tracker
-        )
-    elif tracker == "Commitment":
-        return _check_headers(
-            HEADERS_COMMITMENT_TRACKER,
-            data,
-            tracker
-        )
-    elif tracker == "Disclosure Regime":
-        return _check_headers(
-            HEADERS_DISCLOSURE_REGIMES,
-            data,
-            tracker
-        )
+        return _check_headers(HEADERS_COUNTRY_TRACKER, data, tracker)
+    if tracker == "Commitment":
+        return _check_headers(HEADERS_COMMITMENT_TRACKER, data, tracker)
+    if tracker == "Disclosure Regime":
+        return _check_headers(HEADERS_DISCLOSURE_REGIMES, data, tracker)
+    return False
 
 
 def _check_headers(expected_headers: list, data: dict, tracker: str) -> bool:
@@ -59,14 +63,14 @@ def _check_headers(expected_headers: list, data: dict, tracker: str) -> bool:
     missing = []
     for item in expected_headers:
         try:
-            data['results'][0]['properties'][item]
-        except KeyError as e:
+            data["results"][0]["properties"][item]
+        except KeyError as e:  # noqa: PERF203
             console.warn(e)
             missing.append(item)
 
     if len(missing):
-        subject = f'Found {len(missing)} missing headers in {tracker} tracker...'
-        body = ', '.join(missing)
+        subject = f"Found {len(missing)} missing headers in {tracker} tracker..."
+        body = ", ".join(missing)
         notionbot.fail(subject, body)
         return False
 
@@ -79,11 +83,7 @@ def countries_json():
         countries = CountryTag.objects.all()
         data = []
         for item in countries:
-            data.append({
-                'name': item.name,
-                'iso2': item.iso2,
-                'url': item.url
-            })
+            data.append({"name": item.name, "iso2": item.iso2, "url": item.url})  # noqa: PERF401
         rv = data
     except Exception as e:
         console.warn(e)
@@ -96,19 +96,21 @@ def map_json():
         countries = CountryTag.objects.all()
         data = []
         for item in countries:
-            data.append({
-                'name': item.name,
-                'iso2': item.iso2,
-                'url': item.url,
-                'lat': item.lat,
-                'lon': item.lon,
-                'oo_support': item.oo_support,
-                # 'committed_central': item.committed_central,
-                # 'committed_public': item.committed_public,
-                # 'implementation_central': item.implementation_central,
-                # 'implementation_public': item.implementation_public,
-                'category': item.category,
-            })
+            data.append(  # noqa: PERF401
+                {
+                    "name": item.name,
+                    "iso2": item.iso2,
+                    "url": item.url,
+                    "lat": item.lat,
+                    "lon": item.lon,
+                    "oo_support": item.oo_support,
+                    # 'committed_central': item.committed_central,
+                    # 'committed_public': item.committed_public,
+                    # 'implementation_central': item.implementation_central,
+                    # 'implementation_public': item.implementation_public,
+                    "category": item.category,
+                },
+            )
         rv = data
     except Exception as e:
         console.warn(e)
